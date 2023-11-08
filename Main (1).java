5e76b3241c85417852f19dd8309cb1e8b7432a52
Cruzz112 <143724511+Cruzz112@users.noreply.github.com>
2023-11-07 22:24:14 -0600

    Add files via upload
    EL ejercicio

---
+++ Main (1).java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+        char [] [] letras =
+            {'a','b','c','d','e'},
+            {'f','g','h','i','j'},
+        {'k','m','n','o'}
+    };
+    for (int i = 0; i <3; i++) {
+    for (int j = 0; j < 5; i++) {
+        System.out.print (letras [i][j + "i"])
+            }
+    System.out.println();
+}
